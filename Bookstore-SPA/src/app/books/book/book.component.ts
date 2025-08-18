4b11fc66c11ddcd596487b2cb839496160863f70
Alex <awohlleber@gmail.com>
2025-08-18 17:31:35 -0600

    start of book component

--- Bookstore-SPA/src/app/books/book/book.component.ts
+++ Bookstore-SPA/src/app/books/book/book.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Book } from '../../_models/Book';
+import { BookService } from '../../_services/book.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-book',
@@ -7,5 +12,34 @@ import { Component } from '@angular/core';
   styleUrl: './book.component.css'
 })
 export class BookComponent {
+  book: Book = { id: 0, name: '', author: '', description: '', value: 0, categoryName: '', publishDate: new Date() };
 
+  constructor(
+      private boookService: BookService,
+      private router: Router,
+      private route: ActivatedRoute,
+      private toastr: ToastrService
+    ) {}
+
+    ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = Number(params.get('id'));
+      if (id && id > 0) {
+        this.boookService.getById(id).subscribe({
+          next: (book) => {
+            this.book = book;
+          },
+          error: () => {
+            this.toastr.error('Failed to load book.');
+            this.router.navigate(['/books']);
+          }
+        });
+      }
+    });
+  }
+
+  onSubmit(form: NgForm): void {
+    return;
+    
+  }
 }
